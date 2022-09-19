b5aa04c9f6f3b42371ecf6bed09f310c778a2d6e
fahrulsw <fahrulshalahudin@gmail.com>
2022-09-19 22:52:09 +0700

    first commit tugas5_js

---
+++ tugas5_js/js/calc.js
@@ -0,0 +1,53 @@
+function hitung(btn) {
+  var frm = document.getElementById("calcForm");
+  var a1 = parseFloat(frm.angka1.value);
+  var a2 = parseFloat(frm.angka2.value);
+
+  var tombol = btn;
+
+  switch (tombol) {
+    case "tambah":
+      if (isNaN(a1) || isNaN(a2)) {
+        alert("Harap Masukan Angka");
+      } else {
+        var hitung = a1 + a2;
+        frm.hasil.value = hitung;
+      }
+      break;
+    case "kurang":
+      if (isNaN(a1) || isNaN(a2)) {
+        alert("Harap Masukan Angka");
+      } else {
+        var hitung = a1 - a2;
+        frm.hasil.value = hitung;
+      }
+      break;
+    case "kali":
+      if (isNaN(a1) || isNaN(a2)) {
+        alert("Harap Masukan Angka");
+      } else {
+        var hitung = a1 * a2;
+        frm.hasil.value = hitung;
+      }
+      break;
+    case "bagi":
+      if (isNaN(a1) || isNaN(a2)) {
+        alert("Harap Masukan Angka");
+      } else {
+        var hitung = a1 / a2;
+        frm.hasil.value = hitung;
+      }
+      break;
+    case "pangkat":
+      if (isNaN(a1) || isNaN(a2)) {
+        alert("Harap Masukan Angka");
+      } else {
+        var hitung = Math.pow(a1, a2);
+        frm.hasil.value = hitung;
+      }
+      break;
+
+    default:
+      break;
+  }
+}
